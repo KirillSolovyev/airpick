c95222b41489eac2755b2852fc6c1945c753c384
personal <kirill.solovyev.98@gmail.com>
2022-07-24 20:10:48 +0600

    chore: add font-awesome icons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp, h, resolveComponent } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import router from './router';
 
 import './assets/css/index.css';
+
+library.add(faArrowLeft);
 
 const App = {
   name: 'App',
   render: () => h(resolveComponent('router-view'))
 };
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).component('fa-icon', FontAwesomeIcon).mount('#app');
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <PageLayout>
+    <template #header>
+      <AppHeader class="py-2 bg-blue-500">
+        <template #leftAction>
+          <AppHeaderBackButton
+            class="flex items-center justify-center w-full text-white text-lg"
+          />
+        </template>
+        <h2 class="text-white text-lg font-bold text-center text-ellipsis">
+          Алматы – Нур-Султан
+        </h2>
+        <p class="text-white text-center text-sm">24 авг - 30 авг</p>
+      </AppHeader>
+    </template>
+    <template #default>
+      <section class="mt-5 space-y-3">
+        <SearchResult v-for="i in 10" :key="i" />
+      </section>
+    </template>
+  </PageLayout>
+</template>
+
+<script setup>
+import PageLayout from '@/components/PageLayout.vue';
+import AppHeader from '@/components/header/AppHeader.vue';
+import AppHeaderBackButton from '@/components/header/AppHeaderBackButton.vue';
+import SearchResult from './components/SearchResult.vue';
+</script>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SearchPage'
+});
+</script>
