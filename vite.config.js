7548e8e5245825ce064d4d750b651fb9c90897f9
personal <kirill.solovyev.98@gmail.com>
2022-08-21 14:25:06 +0600

    feat: add pwa library

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,46 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
+import { VitePWA } from 'vite-plugin-pwa';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        'apple-touch-icon.png',
+        'favicon.ico',
+        'favicon-32x32.png'
+      ],
+      manifest: {
+        name: 'Airpick - онлайн покупка авиабилетов',
+        short_name: 'Airpick',
+        description: 'Airpick - покупка билетов онлайн, удобно и быстро',
+        theme_color: '#3b82f6',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
